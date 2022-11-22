737e290e6a8bbc83a6dac27165f81964ddbe4764
AceMadeIt <vn@artblown.io>
2022-11-22 12:31:28 +0100

    my changes

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,24 +9,24 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  contractName: 'ARTblown3',
+  tokenName: 'ARTblown Club',
+  tokenSymbol: 'ABC',
+  hiddenMetadataUri: 'ipfs://QmRoryZ2BJ2CtHEWVSty8cChi3VMLiWUkhLuz1UqQxBUWQ/hidden.json',
   maxSupply: 10000,
   whitelistSale: {
-    price: 0.05,
+    price: 0.000005,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
+    price: 0.000007,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
+    price: 0.000009,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
+  contractAddress: "0x2003C45d6Dd39084C858265e1A048da00C2f9C5E",
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
